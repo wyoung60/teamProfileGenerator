c6dabe351e5f9300846f4799e9ffca96956ce797
Wes Young <wesley.young@du.edu>
2020-12-02 10:09:18 -0700

    Creating individual objects in app.js

--- app.js
+++ app.js
@@ -10,9 +10,126 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let employeeID = 1;
+let employeeArray = [];
+let employeeInfo = {};
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const managerQuestions = [
+  { message: "What is the manager's name? ", name: "name" },
+  { message: "What is the manager's email address? ", name: "email" },
+  { message: "What is the manager's office number? ", name: "officeNumber" },
+  {
+    type: "list",
+    message: "Select the appropriate role of the new employee: ",
+    name: "role",
+    choices: ["Engineer", "Intern", "No more employees to add."],
+  },
+];
+
+const engineerQuestions = [
+  { message: "What is the engineer's name? ", name: "name" },
+  { message: "What is the engineer's email address? ", name: "email" },
+  { message: "What is the engineer's github username? ", name: "github" },
+  {
+    type: "list",
+    message: "Select the appropriate role of the new employee: ",
+    name: "role",
+    choices: ["Engineer", "Intern", "No more employees to add."],
+  },
+];
+
+const internQuestions = [
+  { message: "What is the intern's name? ", name: "name" },
+  { message: "What is the intern's email address? ", name: "email" },
+  { message: "What school does the intern attend? ", name: "school" },
+  {
+    type: "list",
+    message: "Select the appropriate role of the new employee: ",
+    name: "role",
+    choices: ["Engineer", "Intern", "No more employees to add."],
+  },
+];
+
+function managerInquire() {
+  inquirer.prompt(managerQuestions).then((response) => {
+    employeeInfo.name = response.name;
+    employeeInfo.email = response.email;
+    employeeInfo.officeNumber = response.officeNumber;
+    employeeInfo.id = employeeID;
+    employeeInfo.role = "Manager";
+    storeEmployees(employeeInfo);
+    switch (response.role) {
+      case "Engineer":
+        engineerInquire();
+        break;
+      case "Intern":
+        internInquire();
+        break;
+      default:
+        return;
+    }
+  });
+}
+
+managerInquire();
+
+function engineerInquire() {
+  inquirer.prompt(engineerQuestions).then((response) => {
+    employeeInfo = {};
+    employeeInfo.name = response.name;
+    employeeInfo.email = response.email;
+    employeeInfo.github = response.github;
+    employeeInfo.id = employeeID;
+    employeeInfo.role = "Engineer";
+    storeEmployees(employeeInfo);
+    switch (response.role) {
+      case "Engineer":
+        engineerInquire();
+        break;
+      case "Intern":
+        internInquire();
+        break;
+      default:
+        return;
+    }
+  });
+}
+
+function internInquire() {
+  inquirer.prompt(internQuestions).then((response) => {
+    employeeInfo = {};
+    employeeInfo.name = response.name;
+    employeeInfo.email = response.email;
+    employeeInfo.school = response.school;
+    employeeInfo.id = employeeID;
+    employeeInfo.role = "Intern";
+    storeEmployees(employeeInfo);
+    switch (response.role) {
+      case "Engineer":
+        engineerInquire();
+        break;
+      case "Intern":
+        internInquire();
+        break;
+      default:
+        return;
+    }
+  });
+}
+
+function storeEmployees(employee) {
+  let newEmployee = new Manager(
+    employee.name,
+    employee.id,
+    employee.email,
+    employee.officeNumber
+  );
+  employeeArray.push(newEmployee);
+  employeeID++;
+  console.log(employeeArray);
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
