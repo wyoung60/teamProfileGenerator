79fca1405b41ac8aa511cbc82a8ebbb6beb2fe63
Wes Young <wesley.young@du.edu>
2020-12-04 11:59:39 -0700

    Added comments to JS files

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+//Global variables
 let employeeID = 1;
 let employeeArray = [];
 let employeeInfo = {};
 
+//Array of questions for inquirer
 const employeeQuestions = [
   { message: "What is the employee's name? ", name: "name" },
   { message: "What is the employee's email address? ", name: "email" },
@@ -26,20 +28,32 @@ const employeeQuestions = [
   },
 ];
 
+//Function to prompt employee questions.  Takes in the employee's role specific question and role
 function newEmployeeInquire(employeeSpecificQuestion, role) {
+  //Inserts employee's role specific question
   employeeQuestions[2].message = employeeSpecificQuestion;
+  //Inquire prompts questions
   inquirer.prompt(employeeQuestions).then((response) => {
+    //Erases employee array
     employeeInfo = {};
+    //Sets employee name
     employeeInfo.name = response.name;
+    //Sets employee email
     employeeInfo.email = response.email;
+    //Sets employee's unique answer
     employeeInfo.uniqueQuestion = response.uniqueQuestion;
+    //Sets employee id
     employeeInfo.id = employeeID;
+    //Sets employee role
     employeeInfo.role = role;
+    //Moves to function to store employee info into an array.  Takes in the created object and the next employee's role.
     storeEmployees(employeeInfo, response.role);
   });
 }
 
+//Function to store employee object into an array.  Takes in the created object and the next employee's role.
 function storeEmployees(employee, nextEmployee) {
+  //If statements based on employee role creates new class.
   if (employee.role === "Manager") {
     var newEmployee = new Manager(
       employee.name,
@@ -62,8 +76,11 @@ function storeEmployees(employee, nextEmployee) {
       employee.uniqueQuestion
     );
   }
+  //Pushes new employee to array.
   employeeArray.push(newEmployee);
+  //Increments the employee id number for next employee.
   employeeID++;
+  //Switch statement to kick off next set of question for next employee or render the html when completed.
   switch (nextEmployee) {
     case "Engineer":
       newEmployeeInquire(
@@ -74,15 +91,20 @@ function storeEmployees(employee, nextEmployee) {
     case "Intern":
       newEmployeeInquire("What school does the employee attend? ", "Intern");
       break;
+    //Case to call render function.
     case "No more employees to add.":
+      //Store html from render into variable.
       let html = render(employeeArray);
+      //Calls function to create team html file.
       writeHTML(html);
     default:
       return;
   }
 }
 
+//Function to write team html using fs.
 function writeHTML(html) {
+  //Takes in file path, html data, and creates file.  Handles errors as well.
   fs.writeFile(outputPath, html, (err) => {
     if (err) {
       console.log(err);
@@ -92,116 +114,5 @@ function writeHTML(html) {
   });
 }
 
+//Kicks off first set of question for the manager.
 newEmployeeInquire("What is the employee's office number? ", "Manager");
-
-// const managerQuestions = [
-//   { message: "What is the manager's name? ", name: "name" },
-//   { message: "What is the manager's email address? ", name: "email" },
-//   { message: "What is the manager's office number? ", name: "uniqueQuestion" },
-//   {
-//     type: "list",
-//     message: "Select the appropriate role of the new employee: ",
-//     name: "role",
-//     choices: ["Engineer", "Intern", "No more employees to add."],
-//   },
-// ];
-
-// const engineerQuestions = [
-//   { message: "What is the engineer's name? ", name: "name" },
-//   { message: "What is the engineer's email address? ", name: "email" },
-//   {
-//     message: "What is the engineer's github username? ",
-//     name: "uniqueQuestion",
-//   },
-//   {
-//     type: "list",
-//     message: "Select the appropriate role of the new employee: ",
-//     name: "role",
-//     choices: ["Engineer", "Intern", "No more employees to add."],
-//   },
-// ];
-
-// const internQuestions = [
-//   { message: "What is the intern's name? ", name: "name" },
-//   { message: "What is the intern's email address? ", name: "email" },
-//   { message: "What school does the intern attend? ", name: "uniqueQuestion" },
-//   {
-//     type: "list",
-//     message: "Select the appropriate role of the new employee: ",
-//     name: "role",
-//     choices: ["Engineer", "Intern", "No more employees to add."],
-//   },
-// ];
-
-// function managerInquire() {
-//   inquirer.prompt(managerQuestions).then((response) => {
-//     employeeInfo.name = response.name;
-//     employeeInfo.email = response.email;
-//     employeeInfo.officeNumber = response.officeNumber;
-//     employeeInfo.id = employeeID;
-//     employeeInfo.role = "Manager";
-//     storeEmployees(employeeInfo);
-//     switch (response.role) {
-//       case "Engineer":
-//         engineerInquire();
-//         break;
-//       case "Intern":
-//         internInquire();
-//         break;
-//       default:
-//         return;
-//     }
-//   });
-// }
-
-// managerInquire();
-
-// function engineerInquire() {
-//   inquirer.prompt(engineerQuestions).then((response) => {
-//     employeeInfo = {};
-//     employeeInfo.name = response.name;
-//     employeeInfo.email = response.email;
-//     employeeInfo.github = response.github;
-//     employeeInfo.id = employeeID;
-//     employeeInfo.role = "Engineer";
-//     storeEmployees(employeeInfo);
-//     switch (response.role) {
-//       case "Engineer":
-//         engineerInquire();
-//         break;
-//       case "Intern":
-//         internInquire();
-//         break;
-//       case "No more employees to add.":
-//         let html = render(employeeArray);
-//         writeHTML(html);
-//       default:
-//         break;
-//     }
-//   });
-// }
-
-// function internInquire() {
-//   inquirer.prompt(internQuestions).then((response) => {
-//     employeeInfo = {};
-//     employeeInfo.name = response.name;
-//     employeeInfo.email = response.email;
-//     employeeInfo.school = response.school;
-//     employeeInfo.id = employeeID;
-//     employeeInfo.role = "Intern";
-//     storeEmployees(employeeInfo);
-//     switch (response.role) {
-//       case "Engineer":
-//         engineerInquire();
-//         break;
-//       case "Intern":
-//         internInquire();
-//         break;
-//       case "No more employees to add.":
-//         let html = render(employeeArray);
-//         writeHTML(html);
-//       default:
-//         break;
-//     }
-//   });
-// }
